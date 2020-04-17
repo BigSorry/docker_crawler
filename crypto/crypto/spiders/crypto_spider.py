7ca09dcd1883ac3f3bd4979c43a1741c8607eb24
BigSorry <lexmeulenkamp@hotmail.nl>
2020-04-18 01:23:47 +0200

    change extractors for new site

--- crypto/crypto/spiders/crypto_spider.py
+++ crypto/crypto/spiders/crypto_spider.py
@@ -1,32 +1,40 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import datetime
+import re
 
 class CryptoSpider(scrapy.Spider):
     name = 'crypto'
-    start_urls = ['https://coinmarketcap.com/']
+    start_urls = ['https://coinmarketcap.com']
+    currency_pattern = re.compile("^/currencies/[^/]+/$")
+    visited = {}
 
     def parse(self, response):
-        links = response.xpath('//a[@class="currency-name-container link-secondary"]')
+        links = response.xpath('//a[@class="cmc-link"]/@href').extract()
         for link in links:
-            yield response.follow(link, self.goToHistory)
+            if self.currency_pattern.match(link):
+                # base url plus currency url
+                absolute_url = self.start_urls[0] + link
+                if absolute_url not in self.visited:
+                    self.visited[absolute_url] = True
+                    yield response.follow(absolute_url, self.goToHistory)
 
     def goToHistory(self, response):
         # we always need the 5th element
-        li = response.xpath('//ul[@class="nav nav-tabs text-left"]/li')[4]
-        startDate = self.term.strftime("%Y%m%d")
+        li = response.xpath('//ul[@class="cmc-tabs__header"]/li')[4]
+        startDate = "20190101"
         endDate = datetime.date.today().strftime("%Y%m%d")
         dateParam = "?start=" + startDate + "&end=" + endDate
         # only need first result
-        linkHis = li.xpath('a/@href')[0].extract() + dateParam
+        linkHis = self.start_urls[0] + li.xpath('a/@href')[0].extract() + dateParam
         yield response.follow(linkHis, callback=self.getData)
 
     def getData(self, response):
         rows = response.xpath('//tbody/tr')
         for row in rows:
-            items = row.xpath("td/text()")
+            items = row.xpath("td/div/text()")
             coin = {}
-            coin['Name'] = response.xpath('//h1[@class="details-panel-item--name"]/img/@alt').extract()
+            coin['Name'] = response.xpath('//h1/text()').extract()
             coin['Date'] = items[0].extract()
             coin['Market_Cap'] = items[6].extract()
             yield coin
