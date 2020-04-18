9210881fb027a3f0c2f62a68df5486bb5ecfd27f
BigSorry <lexmeulenkamp@hotmail.nl>
2020-04-18 19:47:05 +0200

    dd gitignore plus small bug fix

--- crypto/crypto/__init__.py
+++ crypto/crypto/__init__.py
@@ -1,13 +1,19 @@
-import scrapy
 from scrapy.crawler import CrawlerProcess
 from spiders.crypto_spider import *
+import pandas as pd
 
+ubunut_path = '/home/lex/PycharmProjects/crypto/crypto/crawlUpdate.csv'
+windows_path = '/C:/Users/lexme/PycharmProjects/docker_crawler/crypto/crypto/crawlUpdate.csv'
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
     'FEED_FORMAT': 'csv',
-    'FEED_URI': '/home/lex/PycharmProjects/crypto/crypto/crawlUpdate.csv'
+    'FEED_URI': windows_path
 })
 
-checkDate =  "01 Jan, 2019"
-process.crawl(CryptoSpider(scrapy.Spider), term=checkDate)
-process.start()  # the script will block here until the crawling is finished
+checkDate = "01 Jan, 2019"
+process.crawl(CryptoSpider, term=checkDate)
+process.start()
+
+data = pd.read_csv(windows_path[1:])
+names = data["Name"].unique()
+print(len(names))
